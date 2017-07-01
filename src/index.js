f8e7e08856f5cddf60b9b97cd5cadf83b10b8c31
Scott <scott.mehus@possible.com>
2017-07-01 14:51:05 -0400

    updating intents

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 var Alexa = require('alexa-sdk');
-// WORKS!
-// fuck yeah
+
 exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context, callback);
+      alexa.registerHandlers(handlers);
+	  alexa.execute();
+}
+
+var handlers = {
+	'TestIntent': function () {
+		this.emit(':tell', 'Hi Claire. You look nice. Whatcha gonna make for lunch?');
+	}
 }
